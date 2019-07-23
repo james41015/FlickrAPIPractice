8b2a54f808951d35186954d9dbd5afd931b82ed7
JamesHong <james41015@gmail.com>
2019-07-23 17:32:30 +0800

    favorites features

--- FlickrDemo/ResultCollectionViewCell.swift
+++ FlickrDemo/ResultCollectionViewCell.swift
@@ -12,14 +12,28 @@ import SnapKit
 class ResultCollectionViewCell: UICollectionViewCell {
     var resultImageView = UIImageView()
     var titleLabel = UILabel()
+    var collectButton = UIButton(type: UIButtonType.system)
+    var collectCallBack: (() -> ())?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.titleLabel.textAlignment = .center
+        self.collectButton.setTitle("收藏", for: .normal)
+        self.collectButton.setTitleColor(.white, for: .normal)
+        self.collectButton.backgroundColor = .red
+        self.collectButton.addTarget(self, action: #selector(collectClick), for: UIControlEvents.touchUpInside)
         
         self.contentView.addSubview(resultImageView)
         self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(collectButton)
+        
+        self.collectButton.snp.makeConstraints { (make) in
+            make.height.equalTo(30)
+            make.width.equalTo(40)
+            make.right.equalTo(self.resultImageView.snp.right).offset(0)
+            make.top.equalTo(self.resultImageView.snp.top).offset(0)
+        }
         
         self.resultImageView.snp.makeConstraints { (make) in
             make.left.equalTo(self.contentView).offset(10)
@@ -31,12 +45,15 @@ class ResultCollectionViewCell: UICollectionViewCell {
         self.titleLabel.snp.makeConstraints { (make) in
             make.left.equalTo(self.contentView).offset(10)
             make.right.equalTo(self.contentView).offset(-10)
-//            make.top.equalTo(self.resultImageView.snp.top).offset(10)
             make.bottom.equalTo(self.contentView).offset(-10)
         }
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    @objc private func collectClick() {
+        self.collectCallBack?()
     }
 }
--- FlickrDemo/ResultViewController.swift
+++ FlickrDemo/ResultViewController.swift
@@ -16,8 +16,12 @@ class ResultViewController: UIViewController {
     var text: String! = ""
     var perPage: Int! = 0
     var photosArray = [PhotoModel]()
+    var searchPhotosArray = [PhotoModel]()
+    var favoritesPhotosArray = [PhotoModel]()
     
     let viewModel = ResultViewModel()
+    //tabbar
+    var tabbar: UITabBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,16 +33,17 @@ class ResultViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         self.viewModel.search(text: text, perPage: perPage) { (searchResponseModel, error) in
-            self.photosArray.removeAll()
+            self.searchPhotosArray.removeAll()
             if let searchResponse = searchResponseModel {
                 if let photoModel = searchResponse.photos {
                     if let photos = photoModel.photo {
                         for photo in photos {
-                            self.photosArray.append(photo)
+                            self.searchPhotosArray.append(photo)
                         }
                     }
                 }
             }
+            self.photosArray = self.searchPhotosArray
             self.navigationItem.title = "搜尋結果 \(self.text!)"
             self.resultCollectionView.reloadData()
         }
@@ -55,26 +60,35 @@ class ResultViewController: UIViewController {
         self.resultCollectionView.delegate = self
         self.resultCollectionView.dataSource = self
         self.resultCollectionView.backgroundColor = .white
-
+        
+        self.tabbar = UITabBar()
+        self.tabbar.delegate = self
+        
+        var items = [UITabBarItem]()
+        items.append(UITabBarItem(tabBarSystemItem: .featured, tag: 0))
+        items.append(UITabBarItem(tabBarSystemItem: .favorites, tag: 1))
+        
+        self.tabbar.setItems(items, animated: true)
+        self.tabbar.selectedItem = items[0]
+        
+        self.view.addSubview(self.tabbar)
+        self.tabbar!.snp.makeConstraints { (make) in
+            make.height.equalTo(50)
+            make.bottom.equalTo(self.view.snp.bottomMargin)
+            make.left.equalTo(self.view.snp.left)
+            make.right.equalTo(self.view.snp.right)
+        }
         
         self.view.addSubview(self.resultCollectionView)
         self.resultCollectionView!.snp.makeConstraints { (make) in
             make.top.equalTo(self.view.snp.top)
-            make.bottom.equalTo(self.view.snp.bottom)
+            make.bottom.equalTo(self.tabbar.snp.top)
             make.left.equalTo(self.view.snp.left)
             make.right.equalTo(self.view.snp.right)
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
 
@@ -84,6 +98,7 @@ extension ResultViewController: UICollectionViewDelegate, UICollectionViewDataSo
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ResultCollectionViewCell.self), for: indexPath) as! ResultCollectionViewCell
         //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
         let farmId = self.photosArray[indexPath.row].farm
@@ -93,11 +108,34 @@ extension ResultViewController: UICollectionViewDelegate, UICollectionViewDataSo
         let imageUrl = URL(string: "https://farm\(String(describing: farmId!)).staticflickr.com/\(String(describing: serverId!))/\(String(describing: id!))_\(String(describing: secret!)).jpg")
         cell.resultImageView.kf.setImage(with: imageUrl)
         cell.titleLabel.text = self.photosArray[indexPath.row].title
+        
+        cell.collectButton.isHidden = self.tabbar.selectedItem!.tag == 1
+        
+        cell.collectCallBack = {
+            self.favoritesPhotosArray.append(self.photosArray[indexPath.row])
+        }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         return CGSize(width: self.view.frame.width / 2, height: self.view.frame.width / 2 + 30)
+    }
+    
+}
+
+extension ResultViewController: UITabBarDelegate {
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        switch item.tag {
+        case 0:
+            self.photosArray = self.searchPhotosArray
+            self.navigationItem.title = "搜尋結果 \(self.text!)"
+        case 1:
+            self.photosArray = self.favoritesPhotosArray
+            self.navigationItem.title = "我的最愛"
+        default:
+            break
+        }
+        self.resultCollectionView.reloadData()
     }
 }
